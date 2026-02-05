fdd06da962691f41a36404b2750435cceb14f3b7
le2yunji <le2yunji@naver.com>
2026-02-05 21:43:51 +0900

    2번

--- section1/2. 삼각형 판별하기.js
+++ section1/2. 삼각형 판별하기.js
@@ -1,6 +1,26 @@
+// 길이가 서로 다른 A, B, C 세 개의 막대 길이가 주어지면 이 세 막대로 삼각형을 만들 수 있으면 “YES"를 출력하고, 만들 수 없으면 ”NO"를 출력한다.
+
 function solution(a, b, c) {
   let answer;
+  const sides = [a, b, c];
+  sides.sort((a, b) => a - b);
+  if (sides[2] < sides[0] + sides[1]) answer = "YES";
+  else answer = "NO";
+
   return answer;
 }
 
 console.log(solution(13, 33, 17));
+
+// 강의 정답
+// function solution(a, b, c) {
+//   let answer = "YES",
+//     max;
+//   let sum = a + b + c;
+
+//   if (a > b) max = a;
+//   else max = b;
+//   if (max < c) max = c;
+//   if (sum - max <= max) answer = "NO";
+//   return answer;
+// }
