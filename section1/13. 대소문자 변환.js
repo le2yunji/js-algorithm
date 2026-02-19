e6fd06040ebb98ee4cacee46a32ac6a925c0067e
le2yunji <le2yunji@naver.com>
2026-02-19 19:48:54 +0900

    13번

--- section1/13. 대소문자 변환.js
+++ section1/13. 대소문자 변환.js
@@ -11,3 +11,13 @@ function solution(s) {
 }
 
 console.log(solution("StuDY"));
+
+// 강의 정답
+// function solution(s) {
+//   let answer = "";
+//   for (let x of s) {
+//     if (x === x.toUpperCase()) answer += x.toLowerCase();
+//     else answer += x.toUpperCase();
+//   }
+//   return answer;
+// }
