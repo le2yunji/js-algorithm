33688274f68c62e5aeb4d75dc3580c54c9b871e0
le2yunji <le2yunji@naver.com>
2026-02-19 16:02:53 +0900

    5번

--- section1/5. 최솟값 구하기.js
+++ section1/5. 최솟값 구하기.js
@@ -9,7 +9,7 @@ function solution(arr) {
 let arr = [5, 7, 1, 3, 2, 9, 11];
 console.log(solution(arr));
 
-//  강의 정답
+//  강의 정답 1
 // let answer,
 //   min = Number.MAX_SAFE_INTEGER; // 큰 숫자로 미리 초기화해두기
 // min = arr[0]; // 혹은 배열의 첫 번째 숫자 넣어놓기
@@ -17,4 +17,13 @@ console.log(solution(arr));
 //   if (arr[i] < min) min = arr[i]; // 큰 숫자를 넣어놨기 때문에 첫 번째 숫자는 무조건 들어가게 됨.
 // }
 // answer = min;
+// return answer;
+
+// // 강의 정답 2 - 내장 함수 이용하기
+// let answer = Math.min(arr); // ❌ 괄호 안에 배열같은 객체가 넘어가면 안됨.
+// let answer = Math.min(...arr); //전개 연산자(스프레드 연산자)로 넣어줘야 함.
+// return answer;
+
+// // 강의 정답 3
+// let answer = Math.min.apply(null, arr); //전개 연산자(스프레드 연산자)로 넣어줘야 함.
 // return answer;
