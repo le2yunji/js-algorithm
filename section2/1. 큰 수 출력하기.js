bb5766ca1b0f59b9e5f6be16e0be5bd87896de4c
le2yunji <le2yunji@naver.com>
2026-02-20 17:28:52 +0900

    section2 문제

---
+++ section2/1. 큰 수 출력하기.js
@@ -0,0 +1,10 @@
+// N(1<=N<=100)개의 정수를 입력받아, 자신의 바로 앞 수보다 큰 수만 출력하는 프로그램을 작성하세요.
+// (첫 번째 수는 무조건 출력한다)
+
+function solution(arr) {
+  let answer;
+  return answer;
+}
+
+let arr = [7, 3, 9, 5, 6, 12];
+console.log(solution(arr));
---
+++ section2/5. 등수 구하기.js
@@ -0,0 +1,9 @@
+// N(1<=N<=100)명의 학생의 국어점수가 입력되면 각 학생의 등수를 입력된 순서대로 출력하는 프로그램을 작성하세요.
+
+function solution(arr) {
+  let answer;
+  return answer;
+}
+
+let arr = [87, 89, 92, 100, 76];
+console.log(solution(arr));
